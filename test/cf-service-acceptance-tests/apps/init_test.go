79134be2bbf5dc3f664876863f1754f56b4d8af1
Christopher Jobst and David Sabeti <dsabeti@pivotallabs.com>
2014-02-14 14:10:48 -0800

    Use correct import for cf-test-helpers

--- test/cf-service-acceptance-tests/apps/init_test.go
+++ test/cf-service-acceptance-tests/apps/init_test.go
@@ -4,7 +4,7 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/vito/cmdtest"
-	. "../helpers"
+	. "github.com/pivotal-cf-experimental/cf-test-helpers"
 
 	"testing"
 	"../config"
