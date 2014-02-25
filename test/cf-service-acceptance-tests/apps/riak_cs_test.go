317a8d11fec25640ad1c18d955a0fb62f7b5ea85
Alex Stupakov and Jenny Chou <jchou@pivotallabs.com>
2014-02-25 11:01:04 -0800

    Acceptance test waits for cf commands to return success
    it used to wait for specific messages in output, but this was brittle
    since the cli human-readable output changes.

--- test/cf-service-acceptance-tests/apps/riak_cs_test.go
+++ test/cf-service-acceptance-tests/apps/riak_cs_test.go
@@ -8,17 +8,18 @@ import (
 	. "github.com/pivotal-cf-experimental/cf-test-helpers/cf"
 	. "github.com/pivotal-cf-experimental/cf-test-helpers/generator"
 	"fmt"
+	"time"
 )
 
 var _ = Describe("Riak CS Service", func() {
 	BeforeEach(func() {
 		AppName = RandomName()
 
-		Expect(Cf("push", AppName, "-m", "256M", "-p", sinatraPath, "-no-start")).To(Say("files\nOK"))
+		Expect(Cf("push", AppName, "-m", "256M", "-p", sinatraPath, "-no-start")).To(ExitWithTimeout(0, 60*time.Second))
 	})
 
 	AfterEach(func() {
-		Expect(Cf("delete", AppName, "-f")).To(Say("OK"))
+		Expect(Cf("delete", AppName, "-f")).To(ExitWithTimeout(0, 20*time.Second))
 	})
 
 	It("Allows users to create, bind, write to, read from, unbind, and destroy the service instance", func() {
@@ -26,23 +27,26 @@ var _ = Describe("Riak CS Service", func() {
 		PlanName := "bucket"
 		ServiceInstanceName := RandomName()
 
-		Expect(Cf("create-service", ServiceName, PlanName, ServiceInstanceName)).To(Say("OK"))
-		Expect(Cf("bind-service", AppName, ServiceInstanceName)).To(Say("OK"))
-		Expect(Cf("start", AppName)).To(Say("App started"))
+		Expect(Cf("create-service", ServiceName, PlanName, ServiceInstanceName)).To(ExitWithTimeout(0, 60*time.Second))
+		Expect(Cf("bind-service", AppName, ServiceInstanceName)).To(ExitWithTimeout(0, 60*time.Second))
+		Expect(Cf("start", AppName)).To(ExitWithTimeout(0, 120*time.Second))
 
 		uri := AppUri(AppName) + "/service/blobstore/" + ServiceInstanceName + "/mykey"
 		delete_uri := AppUri(AppName) + "/service/blobstore/" + ServiceInstanceName
 
-		fmt.Println("Posting to url: %s", uri)
+		fmt.Println("Posting to url: ", uri)
 		Eventually(Curling("-d", "myvalue", uri), 10.0, 1.0).Should(Say("myvalue"))
+		fmt.Println("\n")
 
-		fmt.Println("Curling url: %s", uri)
+		fmt.Println("Curling url: ", uri)
 		Eventually(Curling(uri), 10.0, 1.0).Should(Say("myvalue"))
+		fmt.Println("\n")
 
-		fmt.Println("Sending delete to: %s", delete_uri)
+		fmt.Println("Sending delete to: ", delete_uri)
 		Eventually(Curling("-X", "DELETE", delete_uri), 10.0, 1.0).Should(Say(""))
+		fmt.Println("\n")
 
-		Expect(Cf("unbind-service", AppName, ServiceInstanceName)).To(Say("OK"))
-		Expect(Cf("delete-service", "-f", ServiceInstanceName)).To(Say("OK"))
+		Expect(Cf("unbind-service", AppName, ServiceInstanceName)).To(ExitWithTimeout(0, 20*time.Second))
+		Expect(Cf("delete-service", "-f", ServiceInstanceName)).To(ExitWithTimeout(0, 20*time.Second))
 	})
 })
