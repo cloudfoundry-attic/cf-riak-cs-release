76e9c44095694673be4fa2b2489a4be8577a932a
Alex Stupakov and David Sabeti <pair+astupakov+github-pivotal+dsabeti@pivotallabs.com>
2014-07-21 11:48:22 -0700

    riak_backup: exit if s3cmd is not installed

--- scripts/riak-backup/src/riak_backup/main/main.go
+++ scripts/riak-backup/src/riak_backup/main/main.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"riak_backup"
 	"os"
+	"os/exec"
 )
 
 func main() {
 	if len(os.Args) != 5 {
 		printUsage()
 	}
+
+	ensureS3cmdIsInstalled()
 
 	operation := os.Args[1]
 	s3cfg := os.Args[2]
@@ -36,5 +39,14 @@ func main() {
 func printUsage() {
 	fmt.Println("Usage: riak-backup [backup|restore] PATH_TO_S3CFG_FILE CF_ADMIN_USER CF_ADMIN_PASSWORD")
 	os.Exit(1)
+}
+
+func ensureS3cmdIsInstalled() {
+	cmd := exec.Command("which", "s3cmd")
+	_, err := cmd.Output()
+	if err != nil {
+		fmt.Println("Please install s3cmd and make sure it is in the $PATH before running this script.")
+		os.Exit(1)
+	}
 }
 
